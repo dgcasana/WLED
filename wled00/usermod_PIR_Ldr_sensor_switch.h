91196778fbc64530416485a41e08ff0d1f6c4f2b
Daniel Gonzalez <dgcasana@gmail.com>
2021-03-24 10:07:56 +0100

    Improved version of PIR_switch of gegu

--- wled00/usermod_PIR_Ldr_sensor_switch.h
+++ wled00/usermod_PIR_Ldr_sensor_switch.h
@@ -63,30 +63,39 @@ private:
   const uint8_t PIRsensorPin = 13; // D7 on D1 mini
   // notification mode for colorUpdated()
   const byte NotifyUpdateMode = NOTIFIER_CALL_MODE_NO_NOTIFY; // NOTIFIER_CALL_MODE_DIRECT_CHANGE
-  // 1 min delay before switch off after the sensor state goes LOW
-  uint32_t m_switchOffDelay = 60000;
+  // delay before switch off after the sensor state goes LOW
+  uint32_t m_switchOffDelay = 600000;
   // off timer start time
   uint32_t m_offTimerStart = 0;
   // current PIR sensor pin state
   byte m_PIRsensorPinState = LOW;
   // PIR sensor enabled - ISR attached
+  // PIR sensor enabled - ISR attached
+//max time must on with light day
+int t_maxTimeOn = (10*60*1000);
+//max time trggered
+uint32_t t_disablePIR = 0;
+//time to be diseabled
+int t_diseableTime = (60*60*1000);
+//max time triggered
+bool t_maxtimeTrig;
+// PIR sensor enabled
   bool m_PIRenabled = true;
   // state if serializeConfig() should be called
   bool m_updateConfig = false;
   // define analog pin
-  const int LIGHT_PIN = A0; 
-  // current light value
-  int m_lightValue = 0;
-  // there are light enought
-  bool m_lightEnought = false; 
-  // light treshold
-  int m_lightTreshold = 250;
-  // MQTT Topic
-  #define MQTT_TOPIC "/motion"
-  int prevState = LOW;
-  //light activated by PIR
-  bool m_lightActive = false;
-
+const int LIGHT_PIN = A0; 
+// current light value
+int m_lightValue = 0;
+// there are light enought
+bool m_lightEnought = false; 
+// light treshold
+int m_lightTreshold = 250;
+// MQTT Topic
+#define MQTT_TOPIC "/motion"
+int prevState = LOW;
+//light activated by PIR
+bool m_pirActivated = false;
 
   /**
    * return or change if new PIR sensor state is available
@@ -106,20 +115,56 @@ private:
   /**
    * switch strip on/off
    */
-  void switchStrip()
+
+ void switchStrip(bool switchOn)
   {
+      
+  if (switchOn && bri == 0)
+    {
       m_lightEnought = m_lightValue > m_lightTreshold ? true:false;
       if (m_lightEnought){
         applyPreset(random(2,6)); // preset 2-6 para cuando sea de dia.
         colorUpdated(NotifyUpdateMode);
+        
       }
       else{
         applyPreset(1); // El preset 1 sera la luz de noche.
         colorUpdated(NotifyUpdateMode);
       }
+    }
+    else if (!switchOn && bri != 0)
+    {
+      bri = 0;
+      colorUpdated(NotifyUpdateMode);
+    }
   }
 
-  
+  /**
+   * Read and update PIR sensor state.
+   * Initilize/reset switch off timer
+   */
+  bool updatePIRsensorState()
+  {
+    if (newPIRsensorState())
+    {
+      m_PIRsensorPinState = digitalRead(PIRsensorPin);
+      m_lightValue = analogRead(LIGHT_PIN);
+
+      if (m_PIRsensorPinState == HIGH)
+      {
+        m_offTimerStart = 0;
+        switchStrip(true);
+      }
+      else if (bri != 0)
+      {
+        // start switch off timer
+        m_offTimerStart = millis();
+      }
+      newPIRsensorState(true, false);
+      return true;
+    }
+    return false;
+  }
 
   /**
    * switch off the strip if the delay has elapsed 
@@ -130,16 +175,25 @@ private:
     {
       if (m_PIRenabled == true)
       {
-        if (m_lightEnought) bri=0;
-        else{
-        effectCurrent = 38; // aurora?
-        nightlightActive = true; // con esto hace crossfade para apagarse
-        }
+        switchStrip(false);
       }
       m_offTimerStart = 0;
-      m_lightActive = false;
       return true;
     }
+    else if (m_offTimerStart > 0 && (millis() - m_offTimerStart > t_maxTimeOn) && m_lightEnought)
+        {
+          t_maxtimeTrig = true;
+          t_disablePIR = millis();
+          //m_pirActivated = false;
+          switchStrip(false);
+          m_PIRenabled = false;
+          colorUpdated(NotifyUpdateMode);
+        }
+    if (t_maxtimeTrig && (t_disablePIR > 0 && millis() - t_disablePIR > t_diseableTime))
+      {
+        t_maxtimeTrig = false;
+         m_PIRenabled = true;
+      }
     return false;
   }
 
@@ -155,7 +209,6 @@ public:
     // PIR Sensor mode INPUT_PULLUP
     pinMode(PIRsensorPin, INPUT_PULLUP);
     pinMode(LIGHT_PIN,INPUT);
-
     if (m_PIRenabled)
     {
       // assign interrupt function and set CHANGE mode
@@ -171,48 +224,20 @@ public:
   {
   }
 
-  void publishMqtt(String state)
-  {
-  //Check if MQTT Connected, otherwise it will crash the 8266
-    if (mqtt != nullptr)
-    {
-      char subuf[38];
-      strcpy(subuf, mqttDeviceTopic);
-      strcat(subuf, MQTT_TOPIC);
-      mqtt->publish(subuf, 0, true, state.c_str());
-    }
-  }
   /**
    * loop() is called continuously. Here you can check for events, read sensors, etc.
    */
   void loop()
   {
-    if(bri>0) // the lights are off
-    {
-      if (digitalRead(PIRsensorPin) == HIGH && prevState == LOW)
-      { // Motion detected
-        publishMqtt("ON");
-        prevState = HIGH;
-        m_offTimerStart = millis(); // every time move is detected reset the countdown
-        if (!m_lightActive) m_lightValue = analogRead(LIGHT_PIN);
-        m_lightActive = true;
-        switchStrip(); 
-      } 
-    }
-    if (digitalRead(PIRsensorPin) == LOW && prevState == HIGH)
-    {  // Motion stopped
-      publishMqtt("OFF");
-      prevState = LOW;
-    }
-    if(m_lightActive)
+    if (!updatePIRsensorState())
     {
       handleOffTimer();
-    }
-    if (m_updateConfig)
+      if (m_updateConfig)
       {
         serializeConfig();
         m_updateConfig = false;
       }
+    }
   }
 
   /**
@@ -221,32 +246,32 @@ public:
    * Add PIR sensor state and switch off timer duration to jsoninfo
    */
   void addToJsonInfo(JsonObject &root)
+  {
+    //this code adds "u":{"&#x23F2; PIR sensor state":uiDomString} to the info object
+    // the value contains a button to toggle the sensor enabled/disabled
+    JsonObject user = root["u"];
+    if (user.isNull())
+      user = root.createNestedObject("u");
+
+    JsonArray infoArr = user.createNestedArray("&#x23F2; PIR sensor state"); //name
+    String uiDomString = "<button class=\"btn infobtn\" onclick=\"requestJson({PIRenabled:";
+    String sensorStateInfo;
+
+    // PIR sensor state
+    if (m_PIRenabled)
     {
-      //this code adds "u":{"&#x23F2; PIR sensor state":uiDomString} to the info object
-      // the value contains a button to toggle the sensor enabled/disabled
-      JsonObject user = root["u"];
-      if (user.isNull())
-       user = root.createNestedObject("u");
-
-      JsonArray infoArr = user.createNestedArray("&#x23F2; PIR sensor state"); //name
-      String uiDomString = "<button class=\"btn infobtn\" onclick=\"requestJson({PIRenabled:";
-      String sensorStateInfo;
-
-      // PIR sensor state
-      if (m_PIRenabled)
-      {
-        uiDomString += "false";
-        sensorStateInfo = (m_PIRsensorPinState != LOW ? "active" : "inactive"); //value
-      }
-      else
-      {
-        uiDomString += "true";
-        sensorStateInfo = "Disabled !";
-      }
-      uiDomString += "});return false;\">";
-      uiDomString += sensorStateInfo;
-      uiDomString += "</button>";
-      infoArr.add(uiDomString); //value
+      uiDomString += "false";
+      sensorStateInfo = (m_PIRsensorPinState != LOW ? "active" : "inactive"); //value
+    }
+    else
+    {
+      uiDomString += "true";
+      sensorStateInfo = "Disabled !";
+    }
+    uiDomString += "});return false;\">";
+    uiDomString += sensorStateInfo;
+    uiDomString += "</button>";
+    infoArr.add(uiDomString); //value
 
     //this code adds "u":{"&#x23F2; switch off timer":uiDomString} to the info object
     uiDomString = "&#x23F2; switch off timer<span style=\"display:block;padding-left:25px;\">\
@@ -256,37 +281,37 @@ after <input type=\"number\" min=\"1\" max=\"720\" value=\"";
     infoArr = user.createNestedArray(uiDomString); //name
 
     // off timer
-      if (m_offTimerStart > 0)
+    if (m_offTimerStart > 0)
+    {
+      uiDomString = "";
+      unsigned int offSeconds = (m_switchOffDelay - (millis() - m_offTimerStart)) / 1000;
+      if (offSeconds >= 3600)
       {
-        uiDomString = "";
-        unsigned int offSeconds = (m_switchOffDelay - (millis() - m_offTimerStart)) / 1000;
-        if (offSeconds >= 3600)
-        {
-          uiDomString += (offSeconds / 3600); 
-          uiDomString += " hours "; 
-          offSeconds %= 3600;
-        }
-        if (offSeconds >= 60)
-        {
-          uiDomString += (offSeconds / 60); 
-          offSeconds %= 60;
-        }
-        else if (uiDomString.length() > 0)
-        {
-          uiDomString += 0; 
-        }
-        if (uiDomString.length() > 0)
-        {
-          uiDomString += " min ";
-        }
-        uiDomString += (offSeconds); 
-        infoArr.add(uiDomString + " sec");
+        uiDomString += (offSeconds / 3600);
+        uiDomString += " hours ";
+        offSeconds %= 3600;
       }
-      else
+      if (offSeconds >= 60)
       {
-        infoArr.add("inactive");
+        uiDomString += (offSeconds / 60);
+        offSeconds %= 60;
       }
-      //this code adds "u":{"&#x23F2; Ldr sensor data":uiDomString} to the info object
+      else if (uiDomString.length() > 0)
+      {
+        uiDomString += 0;
+      }
+      if (uiDomString.length() > 0)
+      {
+        uiDomString += " min ";
+      }
+      uiDomString += (offSeconds);
+      infoArr.add(uiDomString + " sec");
+    }
+    else
+    {
+      infoArr.add("inactive");
+    }
+    //this code adds "u":{"&#x23F2; Ldr sensor data":uiDomString} to the info object
     uiDomString = "&#x23F2; LDR treshold adjust<span style=\"display:block;padding-left:25px;\">\
 after <input type=\"number\" min=\"1\" max=\"1024\" value=\"";
     uiDomString += (m_lightTreshold);
@@ -296,8 +321,8 @@ after <input type=\"number\" min=\"1\" max=\"1024\" value=\"";
     // m_lightValue
       
       infoArr.add(m_lightValue);
-      
-    }
+
+  }
 
   /**
    * addToJsonState() can be used to add custom entries to the /json/state part of the JSON API (state object).
@@ -327,7 +352,7 @@ after <input type=\"number\" min=\"1\" max=\"1024\" value=\"";
     }
     if (root["LDRadjust"] != nullptr)
     {
-      m_lightTreshold = root["PIRoffSec"].as<int>();
+      m_lightTreshold = max(0, min(1024, root["LDRadjust"].as<int>()));
       m_updateConfig = true;
     }
 
